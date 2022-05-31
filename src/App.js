737eb7e1530d6dd7bf879f185d93009453b0e7c8
DokJS <omarmbengue6919@gmail.com>
2022-05-31 15:46:54 +0000

    app is done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 function App() {
+   const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+  const APIKEY = `70d816fcde4d8cf348cf5b17b57a1845`
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKEY}&units=metric&lang=fr`
+
+  // used for retrieve current location's weather data from API
+  const fetchData = async (event) => {
+    if (event.key === 'Enter') {
+      if (event.target.value !== '') {
+        try {
+        const response = await axios.get(url)
+        setData(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+      } else {
+        // used for remove data when user press enter key when input is empty
+        setData({})
+      }
+    }
+  }
+
+  useEffect(() => {
+    document.title = `My Weather App`
+  },[])
+ 
   return (
     <div className="container">
-      <input type="text" name="location" placeholder='Enter Location' />
-      <div className='main'>
-        <h3>Denver</h3>
-        <h1>30°F</h1>
-      </div>
+      <input type="text" name="location" placeholder='Enter Location' onChange={(event)=>setLocation(event.target.value)} onKeyPress={fetchData} />
+      {
+        data.name &&(<div className='main'>
+        <h3>{data.name }</h3>
+        <h1>{`${data.main?.temp} ° C` } </h1>
+      </div>)
+      }
       <div className='description'>
-        <span>clear</span>
+        <span>{data.weather && data.weather[0].description }</span>
       </div>
-      <div className="informations">
+      {
+        data.name && (<div className="informations">
         <div>
-          <span>30°F</span>
+          <span>{ data.main?.temp}</span>
           <span>Feels Like</span>
         </div>
         <div>
-          <span>72%</span>
+          <span>{ data.main?.humidity }</span>
           <span>Humidity</span>
         </div>
         <div>
-          <span>2 MPH</span>
+          <span>{data.wind?.speed }</span>
           <span>Winds</span>
         </div>
-      </div>
+      </div>)
+      }
     </div>
   );
 }
